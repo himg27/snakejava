ebd62c7dada2b4e11eac30312337ff0b910bf030
andrey.klimenko@gmail.com <andrey.klimenko@gmail.com@0ddb7d4e-8bb1-bd00-ce73-57d51c1a631a>
2011-09-28 15:10:57 +0000

    - added position on board as a separate class - finished snake-move logic (need to be unit-tested) - added an empty(for a while) package for unit-tests

---
+++ trunk/snake/src/snake/model/BoardPosition.java
@@ -0,0 +1,32 @@
+package snake.model;
+
+public class BoardPosition {
+	private int x;
+	private int y;
+	
+	public BoardPosition() {
+		x = 0;
+		y = 0;
+	}
+	
+	public BoardPosition(BoardPosition copy) {
+		x = copy.getX();
+		y = copy.getY();
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+}
