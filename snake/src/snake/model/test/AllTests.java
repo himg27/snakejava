64bc1167c33711e4603d1045b9a19b86477aa667
andrey.klimenko <andrey.klimenko@gmail.com>
2011-09-28 20:53:04 +0000

    added test-suite

---
+++ snake/src/snake/model/test/AllTests.java
@@ -0,0 +1,11 @@
+package snake.model.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CellArrayTest.class, CellTest.class })
+public class AllTests {
+
+}
