78a41ecc9e521525c089c8db5403edd89995274e
andrey.klimenko <andrey.klimenko@gmail.com>
2011-09-29 21:46:42 +0000

    menu

--- snake/src/snake/ui/MainWindow.java
+++ snake/src/snake/ui/MainWindow.java
@@ -1,9 +1,15 @@
 package snake.ui;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 
 import snake.model.Board;
 
@@ -39,5 +45,37 @@ public class MainWindow extends JFrame {
 		}
 		
 		addKeyListener(new KeyboardListener(board.getSnake()));
+		
+        JMenuBar menuBar = new JMenuBar();
+        
+        setJMenuBar(menuBar);
+        
+        JMenu fileMenu = new JMenu("File");
+        menuBar.add(fileMenu);
+        JMenuItem newAction = new JMenuItem("New game");
+        JMenuItem exitAction = new JMenuItem("Exit");
+        exitAction.setAccelerator(KeyStroke.getKeyStroke("alt X"));
+        
+        fileMenu.add(newAction);
+        fileMenu.addSeparator();
+        fileMenu.add(exitAction);
+        
+        newAction.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				newGame();
+			}
+		});
+        
+        exitAction.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				 System.exit(0);				
+			}
+		});
+	}
+	
+	private void newGame() {
+		// TODO: init the snake
 	}
 }
