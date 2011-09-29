b0655e6e3726e7dc834a86a22fb99f6eb68030ce
andrey.klimenko <andrey.klimenko@gmail.com>
2011-09-29 08:10:59 +0000

    - changed name of unit-tests package

---
+++ snake/src/snake/test/SnakeFactory.java
@@ -0,0 +1,37 @@
+package snake.test;
+
+import snake.core.SnakeMoveListenerImpl;
+import snake.model.Snake;
+import snake.model.SnakeCell;
+import snake.model.SnakeCellIterator;
+
+class SnakeFactory {
+	public static int DEFAULT_COORD_VALUE = 5;
+	
+	public static Snake CreateSnake(int size, boolean isHorisontal) {
+		Snake snake = new Snake(size);
+		snake.addSnakeMoveListener(new SnakeMoveListenerImpl(snake));
+		
+		int x = DEFAULT_COORD_VALUE;
+		int y = DEFAULT_COORD_VALUE;
+		
+		if (isHorisontal) {
+			x = snake.getLength();
+		} else {
+			y = snake.getLength();
+		}
+		
+		SnakeCellIterator it = snake.iterator();
+		while (it.hasNext()) {
+			SnakeCell cell = it.next(); 
+			cell.MoveTo(x, y);
+			if (isHorisontal) {
+				--x;
+			} else {
+				--y;
+			}			
+		}
+		
+		return snake;
+	}
+}
