4f5e4470e90be0201c0a8a64a28a430400adc898
andrey.klimenko@gmail.com <andrey.klimenko@gmail.com@0ddb7d4e-8bb1-bd00-ce73-57d51c1a631a>
2011-09-30 22:04:34 +0000

    refactoring: moved exceptions to core sub-package

---
+++ trunk/snake/src/snake/core/SnakeMoveListenerImpl.java
@@ -0,0 +1,63 @@
+package snake.core;
+
+import snake.core.exceptions.InvalidMoveDirectionException;
+import snake.core.exceptions.OutOfBoardBoundsException;
+import snake.model.BoardPosition;
+import snake.model.Snake;
+import snake.model.SnakeCell;
+import snake.model.SnakeDirection;
+import snake.model.SnakeMoveListener;
+import snake.model.collections.SnakeIterator;
+
+
+public class SnakeMoveListenerImpl implements SnakeMoveListener {
+	private Snake snakeObj;
+	private BoardPosition bounds;
+	
+	public SnakeMoveListenerImpl(Snake snake, BoardPosition boardBounds ) {
+		snakeObj = snake;
+		bounds = boardBounds;
+	}
+
+	@Override
+	public void onMove(SnakeDirection direction) throws InvalidMoveDirectionException, OutOfBoardBoundsException {
+		SnakeIterator it = snakeObj.iterator();
+		SnakeCell head = it.next();
+		int oldX = head.getX();
+		int oldY = head.getY();
+		int newX = oldX;
+		int newY = oldY;
+		
+		switch(direction) {
+		case DOWN: 
+			--newY;
+			break;
+		case UP:
+			++newY;
+			break;
+		case LEFT:
+			--newX;
+			break;
+		case RIGHT:
+			++newX;
+			break;
+		}
+
+		if (!snakeObj.isValidDirection(direction))
+			throw new InvalidMoveDirectionException();
+		
+		if (newX > bounds.getX() || newY > bounds.getY())
+			throw new OutOfBoardBoundsException();
+		
+		BoardPosition prev = head.MoveTo(newX, newY);
+		while (it.hasNext()) {
+			SnakeCell cur = it.next();
+			
+			if (!snakeObj.isValidDirection(direction))
+				throw new InvalidMoveDirectionException();
+			
+			prev = cur.MoveTo(prev);
+		}
+	}
+
+}
