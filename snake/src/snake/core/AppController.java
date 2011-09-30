cff0dd18769aadfee0a12f7984e94d808698dd6e
andrey.klimenko <andrey.klimenko@gmail.com>
2011-09-30 22:04:34 +0000

    refactoring: moved exceptions to core sub-package

--- snake/src/snake/core/AppController.java
+++ snake/src/snake/core/AppController.java
@@ -4,12 +4,9 @@ import snake.model.Board;
 import snake.ui.MainWindow;
 
 public class AppController {
-	private MainWindow win;
-	private Board board;
 	
 	public AppController(int c, int r) {	
-		board = new Board(c, r);
-		win = new MainWindow(board);
+		new MainWindow(new Board(c, r));
 	}
 	
 }
